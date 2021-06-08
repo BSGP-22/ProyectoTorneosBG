e864170ec32ee4f9d7b1fd5c299d32093dad2063
Steve <bryanstevedegea@gmail.com>
2021-06-07 23:15:30 -0500

    reporte finalizado

---
+++ Administracion_Torneos/Vista/Reporte_Jugadores.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace Administracion_Torneos.Vista
+{
+    partial class Reporte_Jugadores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listUtilidades = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.listUtilidades)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(300, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 13);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "REPORTE JUGADORES";
+            // 
+            // listUtilidades
+            // 
+            this.listUtilidades.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.listUtilidades.Location = new System.Drawing.Point(31, 95);
+            this.listUtilidades.Name = "listUtilidades";
+            this.listUtilidades.Size = new System.Drawing.Size(757, 259);
+            this.listUtilidades.TabIndex = 22;
+            // 
+            // Reporte_Jugadores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listUtilidades);
+            this.Controls.Add(this.label1);
+            this.Name = "Reporte_Jugadores";
+            this.Text = "Reporte_Jugadores";
+            ((System.ComponentModel.ISupportInitialize)(this.listUtilidades)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView listUtilidades;
+    }
+}
---
+++ Administracion_Torneos/Vista/Reporte_Jugadores.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Administracion_Torneos.Vista
+{
+    public partial class Reporte_Jugadores : Form
+    {
+        public Reporte_Jugadores()
+        {
+            InitializeComponent();
+        }
+    }
+}
